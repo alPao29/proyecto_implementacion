c1ade7c9661f40b9b6b03a1d5b13af4ad18742ea
justin2070 <43504797+justin2070@users.noreply.github.com>
2020-11-07 10:11:17 -0600

    implementacion de plantilla

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
+Route::get('/admin', function () {
+    return view('admin');
+});
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
